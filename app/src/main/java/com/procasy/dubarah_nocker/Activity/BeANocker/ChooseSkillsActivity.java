f1b890c67d735f61f7dd26ce6dadda23400573f6
ahmadalammar <ahmad1costi@gmail.com>
2016-07-30 10:38:24 +0300

    styles_2

--- app/src/main/java/com/procasy/dubarah_nocker/Activity/BeANocker/ChooseSkillsActivity.java
+++ app/src/main/java/com/procasy/dubarah_nocker/Activity/BeANocker/ChooseSkillsActivity.java
@@ -1,32 +1,58 @@
 package com.procasy.dubarah_nocker.Activity.BeANocker;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.procasy.dubarah_nocker.API.APIinterface;
+import com.procasy.dubarah_nocker.API.ApiClass;
 import com.procasy.dubarah_nocker.Adapter.AdapterCallback;
 import com.procasy.dubarah_nocker.Adapter.SkillsAdapter;
+import com.procasy.dubarah_nocker.Helper.SessionManager;
+import com.procasy.dubarah_nocker.Helper.Skills;
+import com.procasy.dubarah_nocker.Model.Responses.AllSkillsResponse;
 import com.procasy.dubarah_nocker.R;
 import com.zhy.view.flowlayout.FlowLayout;
 import com.zhy.view.flowlayout.TagAdapter;
 import com.zhy.view.flowlayout.TagFlowLayout;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import cc.cloudist.acplibrary.ACProgressConstant;
+import cc.cloudist.acplibrary.ACProgressFlower;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ChooseSkillsActivity extends AppCompatActivity implements AdapterCallback {
+
+    Skills mskills;
+    SessionManager sessionManager;
     RecyclerView skill_list;
     TagFlowLayout chosen_skills;
     ArrayList<String> chosenSkills = new ArrayList<>();
     ArrayList<String> skills = new ArrayList<>();
     private AdapterCallback mAdapterCallback;
+
+    private List<String> GetAllSkills()
+    {
+
+        return skills;
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,12 +69,13 @@ public class ChooseSkillsActivity extends AppCompatActivity implements AdapterCa
         skills.add(new String("Cook"));
         skills.add(new String("Maid"));
         skills.add(new String("Nanny"));
-
+        sessionManager = new SessionManager(this);
+        mskills = new Skills(this);
 
         chosenSkills.add(new String("Piano Lessons"));
         chosenSkills.add(new String("Tap Dancing"));
 
-        skill_list.setAdapter(new SkillsAdapter(getApplicationContext(), skills,mAdapterCallback));
+        skill_list.setAdapter(new SkillsAdapter(getApplicationContext(), skills, mAdapterCallback));
         chosen_skills.setAdapter(new TagAdapter<String>(chosenSkills)
         {
             @Override
@@ -61,6 +88,58 @@ public class ChooseSkillsActivity extends AppCompatActivity implements AdapterCa
                 return tv;
             }
         });
+
+
+
+        final ACProgressFlower dialog = new ACProgressFlower.Builder(this)
+                .direction(ACProgressConstant.DIRECT_CLOCKWISE)
+                .themeColor(Color.WHITE)
+                .text("Getting Info..")
+                .fadeColor(Color.DKGRAY).build();
+        dialog.show();
+
+
+        APIinterface apiService = ApiClass.getClient().create(APIinterface.class);
+        Call<AllSkillsResponse> call = apiService.GetAllSkills(sessionManager.getEmail(), sessionManager.getPassword());
+        call.enqueue(new Callback<AllSkillsResponse>() {
+            @Override
+            public void onResponse(Call<AllSkillsResponse> call, Response<AllSkillsResponse> response) {
+
+                mskills.open();
+                Log.e("remove state ", mskills.removeAllEntry() + "");
+
+                skills.clear();
+                try {
+                    for (int i = 0; i < response.body().getAllSkills().size(); i++) {
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(Skills.COL_skillname, response.body().getAllSkills().get(i).getSkill_name());
+                        contentValues.put(Skills.COL_is_hidden, response.body().getAllSkills().get(i).getIs_hidden());
+                        contentValues.put(Skills.COL_SKILL_ID, response.body().getAllSkills().get(i).getSkill_id());
+                        long state = mskills.insertEntry(contentValues);
+                        Log.e("insert state ", state + "");
+                        skills.add(response.body().getAllSkills().get(i).getSkill_name());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    mskills.close();
+                }
+
+
+                if (dialog.isShowing())
+                    dialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<AllSkillsResponse> call, Throwable t) {
+                System.out.println("ERROR 2" + t.toString());
+                if (dialog.isShowing())
+                    dialog.dismiss();
+            }
+
+        });
+
     }
 
     @Override
