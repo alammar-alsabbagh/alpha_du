5e132080729b567ef50a6ddb0ac8de99cba3f66b
ahmadalammar <ahmad1costi@gmail.com>
2016-07-31 13:35:48 +0300

    Profile

---
+++ app/src/main/java/com/procasy/dubarah_nocker/Model/AboutModel.java
@@ -0,0 +1,13 @@
+package com.procasy.dubarah_nocker.Model;
+
+/**
+ * Created by DELL on 31/07/2016.
+ */
+public class AboutModel {
+
+    public String data;
+
+    public AboutModel(String data) {
+        this.data = data;
+    }
+}
